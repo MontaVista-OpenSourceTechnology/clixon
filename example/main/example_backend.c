ec0a36fce1407c51888f5e7eaa2393d48ec59ae8
Corey Minyard <corey@minyard.net>
2024-10-16 10:30:14 -0500

    Fix a typo
    Signed-off-by: Corey Minyard <corey@minyard.net>

--- example/main/example_backend.c
+++ example/main/example_backend.c
@@ -677,7 +677,7 @@ example_statefile(clixon_handle     h,
  * @retval    -1   Error
  *
  * System-only config data as defined by _ is not written to datastore.
- * Instead, in this ocmmit action, it is written to file _state_file
+ * Instead, in this commit action, it is written to file _state_file
  * @see main_system_only_commit  callback for reading data
  * @note  Only single system-only config data supported
  */
